ac49b759ce638157fe9f5a6bac72dcc401780bf5
Kang Jisoon <wltnskjs@naver.com>
2024-02-04 16:53:02 +0900

    ✨ Change Resolution

--- death-stranding-resoultion-setting/death-stranding-resoultion-setting/ContentView.swift
+++ death-stranding-resoultion-setting/death-stranding-resoultion-setting/ContentView.swift
@@ -10,9 +10,16 @@ import UniformTypeIdentifiers
 
 
 struct ContentView: View {
-    @State var isShowing = false
-    @State var isFileOpen = false
+    @State var isShowing: Bool = false
+    @State var isFileOpen: Bool = false
     @State var configValue: [String: String] = [:]
+    @State var openFilePath: URL?
+    @State var fileText: String = ""
+    
+    @State var edited_height: String = ""
+    @State var edited_width: String = ""
+    @State var origin_height: String = ""
+    @State var origin_width: String = ""
     
     var body: some View {
         
@@ -30,16 +37,19 @@ struct ContentView: View {
                     Text("Open file")
                 })
             }
-            List {
-                ForEach(configValue.sorted(by: <), id: \.key) { key, value in
-                    Text("\(key): \(value)")
-                }
-            }
+            Spacer()
+            Text("Display Resoultion")
+                .font(.headline)
+            HStack(){
+                TextField("width", text: $edited_width)
+                Text("x")
+                TextField("height", text: $edited_height)
+            }.disabled(false)
             
             Button(action: {
-                changeData()
+                saveFile()
             }, label: {
-                Text("HDR off")
+                Text("Save")
             })
             .disabled(false)
             
@@ -47,8 +57,28 @@ struct ContentView: View {
         }
         .padding()
     }
-    func changeData() {
-        configValue.updateValue("\"0\"", forKey: "\"hdr\"")
+    func combinePrefixAndSuffix(pre: String, suf: String) -> String {
+        return pre + "\"" + suf + "\""
+    }
+    
+    func saveFile() {
+        if let fileURL = openFilePath {
+            do {
+                let prefix_width: String = "\"rendering_width\" "
+                let prefix_height: String = "\"rendering_height\" "
+                let origin_width_line: String = combinePrefixAndSuffix(pre: prefix_width, suf: origin_width)
+                let origin_height_line: String = combinePrefixAndSuffix(pre: prefix_height, suf: origin_height)
+                let edited_width_line: String = combinePrefixAndSuffix(pre: prefix_width, suf: edited_width)
+                let edited_height_line: String = combinePrefixAndSuffix(pre: prefix_height, suf: edited_height)
+                
+                fileText = fileText.replacingOccurrences(of: origin_width_line, with: edited_width_line)
+                fileText = fileText.replacingOccurrences(of: origin_height_line, with: edited_height_line)
+                
+                try fileText.write(to: fileURL, atomically: true, encoding: .utf8)
+            } catch let e {
+                print(e.localizedDescription)
+            }
+        }
         print("change!")
         
         
@@ -64,22 +94,35 @@ struct ContentView: View {
         openPanel.canChooseDirectories = false
         openPanel.canCreateDirectories = false
         openPanel.allowedContentTypes = [.cfg]
+        
         openPanel.begin { response in
             if response.rawValue == NSApplication.ModalResponse.OK.rawValue {
                 if let fileURL = openPanel.url {
                     do {
-                        var fileText = try String(contentsOf: fileURL)
+                        fileText = try String(contentsOf: fileURL)
+                        openFilePath = fileURL
                         
                         let getLines = fileText.components(separatedBy: .newlines)
-                        
                         for line in getLines {
-                            let components = line.components(separatedBy: .whitespaces)
-                            if (components.count == 2) {
-                                let key = components[0]
-                                let value = components[1]
-                                configValue[key] = value
+                            if (line.contains("rendering_width"))
+                            {
+                                let components = line.components(separatedBy: .whitespaces)
+                                if (components.count == 2) {
+                                    origin_width = components[1].replacingOccurrences(of: "\"", with: "")
+                                    edited_width = origin_width
+                                }
                             }
+                            if (line.contains("rendering_height"))
+                            {
+                                let components = line.components(separatedBy: .whitespaces)
+                                if (components.count == 2) {
+                                    origin_height = components[1].replacingOccurrences(of: "\"", with: "")
+                                    edited_height = origin_height
+                                }
+                            }
+                            
                         }
+                        
                         
 //                        fileText.append("\nHello World")
 //                        try fileText.write(to: fileURL, atomically: true, encoding: .utf8)
