74f6c96ae9e493c1d85af4c99681cb947822daca
Kang Jisoon <wltnskjs@naver.com>
2024-02-06 15:54:58 +0900

    💄 Improve App UI

--- death-stranding-resoultion-setting/death-stranding-resoultion-setting/Views/ContentView.swift
+++ death-stranding-resoultion-setting/death-stranding-resoultion-setting/Views/ContentView.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 import UniformTypeIdentifiers
 
-
 struct ContentView: View {
     @State var isShowing: Bool = false
     @State var isFileOpen: Bool = false
+    @State public var isHelpWindowOpen: Bool = false
     
     @State var openFilePath: URL?
     @State var fileText: String = ""
@@ -21,7 +21,9 @@ struct ContentView: View {
     @State var origin_height: String = ""
     @State var origin_width: String = ""
     
-    @State var selected_num: Int = 1
+    @State var selected_num: Int = 0
+    
+    @State private var availableResolutions: [String] = []
     
     let rendering_width: String = "rendering_width"
     let rendering_height: String = "rendering_height"
@@ -29,63 +31,140 @@ struct ContentView: View {
     var body: some View {
         
         VStack(alignment: .leading) {
-            
-            HStack(){
-                Text("Config file data")
-                    .font(.headline)
-                    .multilineTextAlignment(.leading)
+            HStack(alignment: .bottom){
+                VStack(alignment: .leading){
+                    Text("16:10 Display Setting Tool")
+                        .font(.title)
+                        .multilineTextAlignment(.leading)
+                        .padding(.trailing)
+                    Text("DEATH STRANDING Director's Cut for macOS")
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
+                        .padding(.bottom)
+                }.padding([.top, .leading, .bottom])
                 Spacer()
-                Button(action: {
-                    isShowing.toggle()
-                    openFile()
-                }, label: {
-                    Text("Open file")
-                })
             }
-            Spacer()
-            Divider()
-            
-                
-            Text("Select Mac Screen")
-                .font(.headline)
-            Picker(selection: $selected_num, label: Text("Select My Mac")) {
-                Text("MacBook Air 13-inch with M1").tag(1)
-                Text("MacBook Air 13-inch with M2").tag(2)
-                Text("MacBook Air 15-inch").tag(3)
-                Divider()
-                Text("MacBook Pro 14-inch").tag(4)
-                Text("MacBook Pro 16-inch").tag(5)
-                Divider()
-                Text("Custom").tag(6)
-            }
-            .disabled(!isFileOpen)
             
             
-            Text("Display Resolution")
-                .font(.headline)
-            HStack(){
-                TextField("width", text: $edited_width)
-                Text("x")
-                TextField("height", text: $edited_height)
-            }.disabled(!isFileOpen)
             
-            HStack(){
-                Button(action: {
-                    saveFile()
-                }, label: {
-                    Text("Save")
+            GroupBox(){
+                VStack(alignment: .leading){
+                    Text("Open Setting File")
+                        .font(.body)
+                        .multilineTextAlignment(.leading)
+                    Text("Open the 'Setting.cfg' file to change the resolution and aspect ratio.")
+                        .font(.subheadline)
+                        .foregroundColor(Color.gray)
+           
+//                    TextField("", text: .constant("/Users/{USER_NAME}/Library/Containers/com.505games.deathstranding/Data/Setting.cfg"))
+//                        .font(.subheadline)
+//                        .disabled(/*@START_MENU_TOKEN@*/true/*@END_MENU_TOKEN@*/)
+                    
+                    
+                    Divider()
+                    HStack(){
+                        Spacer()
+                        if (isFileOpen){
+                            Image(systemName: "checkmark.circle.fill")
+                                .foregroundColor(Color.green)
+                        }
+                        Button(action: {
+                            isShowing.toggle()
+                            openFile()
+                        }, label: {
+                            Text("Open file...")
+                        })
+                    }
+                    .padding(.top, -1.0)
+                }
+                .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+            }.padding([.leading, .trailing])
+            
+            
+            GroupBox(){
+                Picker(selection: $selected_num, label: Text("Select display resolution")) {
+                    Text("Custom").tag(0)
+                    Divider()
+                    Text("3456 x 2160 (MacBook Pro 16-inch)").tag(1)
+                    Text("3024 x 1890 (MacBook Pro 14-inch)").tag(2)
+                    Text("2880 x 1800 (MacBook Air 15-inch)").tag(3)
+                    Text("2560 x 1600 (MacBook Air 13-inch)").tag(4)
+                    Text("1680 x 1050").tag(5)
+                    Text("1280 x 800").tag(6)
+                    
+                }
+                .onChange(of: selected_num, {
+                    selectResolution(num: selected_num)
                 })
                 .disabled(!isFileOpen)
-                if (!isFileOpen) {
-                    Text("Need to open the Setting.cfg file")
-                        .font(.caption)
-                        .foregroundColor(Color.gray)
+                .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                
+                HStack(){
+                    TextField("width", text: $edited_width)
+                    Text("x")
+                    TextField("height", text: $edited_height)
                 }
+                .disabled(!isFileOpen || selected_num != 0)
+                .padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                
+                HStack(){
+                    Button(action: {
+                        saveFile()
+                    }, label: {
+                        Text("Apply")
+                    })
+                    .disabled(!isFileOpen)
+                    if (!isFileOpen) {
+                        Text("Need to open the Setting.cfg file")
+                            .font(.caption)
+                            .foregroundColor(Color.gray)
+                    }
+                    Spacer()
+                }.padding(/*@START_MENU_TOKEN@*/.all, 5.0/*@END_MENU_TOKEN@*/)
+                
+                
+            }.padding(.all)
+            
+            Button("Help") {
+                isHelpWindowOpen.toggle()
+                
             }
+            .padding([.leading, .bottom])
+            .popover(isPresented: $isHelpWindowOpen, content: {
+                HelpView()
+            })
+            
+            
         }
         .padding()
-        .frame(minWidth: 400, minHeight: 300)
+        
     }
+    
+    func selectResolution(num:Int) {
+        switch num {
+        case 1:
+            edited_width = "3456"
+            edited_height = "2160"
+        case 2:
+            edited_width = "3024"
+            edited_height = "1890"
+        case 3:
+            edited_width = "2880"
+            edited_height = "1800"
+        case 4:
+            edited_width = "2560"
+            edited_height = "1600"
+        case 5:
+            edited_width = "1680"
+            edited_height = "1050"
+        case 6:
+            edited_width = "1280"
+            edited_height = "800"
+        default:
+            print("no resolution in list")
+        }
+    }
+   
     
     func saveFile() {
         if let fileURL = openFilePath {
