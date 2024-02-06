9f3f5e3e92120a01afd81013ca344ad55c2e4976
Kang Jisoon <wltnskjs@naver.com>
2024-02-06 15:53:26 +0900

    🧪 Test HelpView

---
+++ death-stranding-resoultion-setting/death-stranding-resoultion-setting/Views/HelpView.swift
@@ -0,0 +1,20 @@
+//
+//  HelpView.swift
+//  death-stranding-resoultion-setting
+//
+//  Created by 강지순 on 2/6/24.
+//
+
+import SwiftUI
+
+struct HelpView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).padding()
+        
+
+    }
+}
+
+#Preview {
+    HelpView()
+}
